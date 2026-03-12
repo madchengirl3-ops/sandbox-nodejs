60fbe7825b9671fcbcfe4e810717cac71bc5cb99
Anna Kalugina <madchengirl3@gmail.com>
2026-03-12 17:27:51 +0300

    Add ex03, ex04 and ex05

---
+++ ex04_async_cjs/index.js
@@ -0,0 +1,30 @@
+function TestFunction() {
+    //return "Hello!";
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        //resolve("Hello!");
+        reject ("Some error!");
+    }, 5000);
+}); }
+
+console.log("App Started!");
+
+/*const testValue = TestFunction();
+
+testValue.then((testValue) => {
+    console.log("Res = ", testValue);
+}).catch((err) => {
+    console.log("Err = ", err);
+}).finally(() => {
+    console.log("Finally detected!");
+});*/
+
+(async () => {
+   try {
+    const testValue = await TestFunction();
+   console.log("Res=", testValue);
+   } catch (err) {
+    console.log("Err = ", err);
+   }
+   console.log("Finally detected!");
+}) ();
